b3518492f5fc4d403a15eed8e52c180b43214464
Corbin Robb <corbin@Corbins-MacBook-Pro.local>
2020-10-07 11:24:10 -0600

    chore: Add more profile seeds and fix typos in others

--- data/seeds/002_topics.js
+++ data/seeds/002_topics.js
@@ -7,7 +7,7 @@ exports.seed = function (knex) {
       return knex('topics').insert([
         {
           created_by: '00ulthapbErVUwVJy4x6',
-          frequency: 'daily',
+          frequency: 'Daily',
           title: 'Development Team',
         },
       ]);
